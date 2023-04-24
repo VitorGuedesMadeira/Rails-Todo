7c89cde07efd94b814a07da13b199fdea6a401a7
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-04-24 16:47:05 -0300

    Add NEW and CREATE functionalities

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,31 @@
 class TodosController < ApplicationController
-  def index; end
+  def index
+    @todos = Todo.all.order(id: :asc)
+  end
 
-  def new; end
+  def new
+    @todo = Todo.new
+  end
 
-  def create; end
+  def create
+    @todo = Todo.new(params_required)
+
+    if @todo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def params_required
+    params.require(:todo).permit(:description)
+  end
 end
